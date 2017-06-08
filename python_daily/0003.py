549aef4d0b69858f87fb6ea62c1b405cde8b52df
wjsaya@isoft <sayawj@163.com>
2017-06-08 14:24:54 +0800

    修改：      0002.py 	新文件：    0003.py

--- python_daily/0003.py
+++ python_daily/0003.py
@@ -1,9 +1,9 @@
 #coding: utf-8
-#第 0002 题：将 0001 题生成的 200 个激活码（或者优惠券）保存到 MySQL 关系型数据库中。
+#**第 0003 题：**将 0001 题生成的 200 个激活码（或者优惠券）保存到 Redis 非关系型数据库中。
 #Auther: wjsaya
 from random import choice
 import string
-import pymysql.cursors
+import redis
        
 def get_code(dict, length, count):
 #根据给定字典，长度来得出激活码
@@ -12,25 +12,18 @@ def get_code(dict, length, count):
     #通过count限制激活码个数，循环调用choice来计算激活码
         for l in range(0,int(length)):
             code = code+str(choice(dict))
-        save_to_mysql(i, code)
-    
-def save_to_mysql(id, code):
-#保存到mysql数据库
-    host = ("192.168.122.18")
-    user = ("root")
-    pass_ = ("123qwe")
-    db = ("active")
-    #设置数据库连接相关信息
-    connect = pymysql.connect(host, user, pass_, db)
-    cursor = connect.cursor()
-    #链接数据库并设置游标
-    sql = "insert into activeCode(id, code) VALUES ('%d', '%s')"
-    data = (id, code)
-    cursor.execute(sql % data)
-    #执行sql语句
+        save_to_redis(count, code)
+
+def save_to_redis(max, value):
+    #保存到redis数据库
+    redisdb = redis.Redis(host='10.2.2.131',port=6379,db=0)
+    redisdb.lpush('active_code',value)
+    redisdb.lrange('active_code',0,max)
+    redisdb.save()
+
     
 if __name__ == "__main__":
-     dict = string.ascii_letters[:]
+    dict = string.ascii_letters[:]
     #设定字典为'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
     count = input("请输入激活码个数：")
     if count == "":
