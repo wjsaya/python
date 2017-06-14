3859693cff67456439de3d6164e5a2a38328d44d
wjsaya <wjsaya@users.noreply.github.com>
2017-06-14 16:20:32 +0800

    获取猫片
    获取猫片

--- spiders/cat.py
+++ spiders/cat.py
@@ -2,6 +2,7 @@
 import urllib.request
 
 response = urllib.request.urlopen('http://placekitten.com/g/700/700')
+#700/700表示图片大小为700*700，可任意更改
 cat_img = response.read()
 
 with open ('cat_700_700.jpg', 'wb') as cat:
