857c40bb468710aff64bd98a47e3a7a7ab748ffd
wjsaya <wjsaya@users.noreply.github.com>
2017-06-14 16:21:34 +0800

    从豆瓣音乐爬取排名前X的专辑

--- spiders/douban_music.py
+++ spiders/douban_music.py
@@ -7,7 +7,7 @@ url = "https://music.douban.com/top250?start=25"
 firefox={"User-Agent":"Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0 FirePHP/0.7.4"}
 
 def main():
-    maxm = input("想获取豆瓣排名前多少的音乐列表呢？(0-250之间）：")
+    maxm = input("想获取豆瓣排名前多少的专辑列表呢？(0-250之间）：")
     count_main = 0
     url_li = get_pages()
     for i in range(0,9):
