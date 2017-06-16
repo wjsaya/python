5dfe5417a673870115d38a112b28a2189619b41f
wjsaya@isoft <sayawj@163.com>
2017-06-16 17:45:51 +0800

    重命名：    00000.py -> 0000.py 	新文件：    0001.py 	新文件：    activecode.code

---
+++ python_daily/0001.py
@@ -0,0 +1,38 @@
+#coding: utf-8
+#Auther: wjsaya
+from random import choice
+import string
+import os
+
+def main():
+    if os.path.exists("./activecode.code"):
+        print ("已经存在activecode.code文件，已经删除")
+        os.remove("./activecode.code")
+
+    dict = string.ascii_letters[:]
+    #设定字典为'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    count = input("请输入激活码个数：")
+    if count == "":
+        count = "1"
+    
+    length = input("请输入激活码长度：")
+    if length == "":
+        length = "8"
+   
+    for i in range(0,int(count)):
+        code = get_code(dict, length)
+        with open ('activecode.code', 'a+') as f:
+            f.write(code+'\n')
+    #通过count限制激活码个数，循环调用get_code来计算激活码 
+
+def get_code(dict, length):
+    #根据给定字典，长度来得出激活码
+    code = ""
+    for i in range(0,int(length)):
+        code = code+str(choice(dict))
+    return code
+ 
+
+if __name__ == "__main__":
+    main()
+
