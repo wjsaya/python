abf6937eada5cbda342762c53941c191fc925018
wjsaya <wjsaya@win7>
2017-06-19 10:11:15 +0800

    0001

--- python_daily/0001.py
+++ python_daily/0001.py
@@ -1,6 +1,5 @@
 #coding: utf-8
-#第 0002 题：将 0001 题生成的 200 个激活码（或者优惠券）保存到 MySQL 关系型数据库中。
-#Auther: wjsaya
+#第 0001 题：做为 Apple Store App 独立开发者，你要搞限时促销，为你的应用生成激活码（或者优惠券），使用 Python 如何生成 200 个激活码（或者优惠券）#Auther: wjsaya
 from random import choice
 import string
 import os
