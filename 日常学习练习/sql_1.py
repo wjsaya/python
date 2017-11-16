ebd323b8561227249f0cca90de782b58a9182124
wjsaya@centos7 <sayawj@163.com>
2017-11-16 18:20:09 +0800

    new file:   "\346\227\245\345\270\270\345\255\246\344\271\240\347\273\203\344\271\240/sql_1.py"

---
+++ 日常学习练习/sql_1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+#!coding: utf-8
+
+import pymysql
+
+conn = pymysql.connect(host='localhost',port=3306,user='root',password='T_DPwdO3')
+
+conn.query("drop database test")
+conn.query("create database test")
+conn.query("use test")
+conn.query("create table t(row1 int,row2 int,row3 varchar(10))")
+conn.commit()
+
+cur=conn.cursor()
+for row1 in range(0,9):
+    for row2 in range(0,9):
+        for row3 in range(0,9):
+            cur.execute("insert into t values(row1,row2,row3)")
+
+cur.execute("select * from t")
+cur.close()
+conn.commit()
+conn.close()        
+
+
+
