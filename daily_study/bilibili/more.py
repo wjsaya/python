f208dad74688331d048adbddfadc98b08aea62ad
saya_win10 <sayawj@163.com>
2017-11-20 14:37:19 +0800

    windows done

--- daily_study/bilibili/more.py
+++ daily_study/bilibili/more.py
@@ -1,13 +1,44 @@
 #!/use/bin/env python3
 #encoding: utf-8
 
-import multiprocessing
+from multiprocessing import Pool
 import time
-
-def process(num):
-    print ("process%d" % num)
+import multiprocessing
+import pymysql
+def insert_waiting(u_sql, p_sql, mid, i):
+    print ("%s进程已启动,开始处理mid为%s的用户" % (multiprocessing.current_process().name, i))
+    time.sleep(1)
+    conn = pymysql.connect(host='localhost',port=3306,user=u_sql,password=p_sql, db='bilibili', charset="utf8")
+    row = conn.cursor()
+    insert_into_relation = "insert into up_relation values(%d,%d);"
+    insert_into_waiting  = "insert into up_waiting values(%d,%d);"
+    get_info_from_up_info = "select * from up_info where mid = %d;"
+    try:
+        row.execute(insert_into_relation % (int(mid),i))
+    except Exception as e:
+        print ("此对应关系已记录？报错如下：")
+        print (e)
+    conn.commit()
+    try:
+        print (row.execute(get_info_from_up_info % i))
+        print ("id为%d的用户已抓取，不用再次入库" % i)
+    except Exception as e:
+        row.execute(insert_into_waiting % (int(), i))
+        print ("UPID=%d已加入waiting库，待抓取" % i)
+        conn.commit()
+    row.close()
+    conn.close()
 
 if __name__ == '__main__':
-    for i in range(1,100)
-        p = mulyiprocessing.Process(target=process, args=(i,))
-        p.start()
+    
+    u_sql = 'pylocal'
+    p_sql = '123456'
+    mid = 592761
+    flowing = Pool(2)
+    for i in range(1,6):
+        flowing.apply_async(func=insert_waiting, args=(u_sql, p_sql, mid, i,))
+
+    print ('main ended')
+    flowing.close()
+    flowing.join()
+
--- daily_study/bilibili/sqlconn.py
+++ daily_study/bilibili/sqlconn.py
@@ -2,6 +2,9 @@
 #encoding:utf-8
 import pymysql
 import time
+from multiprocessing import Pool
+import multiprocessing
+import requests
 
 def db_init(u_sql, p_sql):
     conn = pymysql.connect(host='localhost',port=3306,user=u_sql,password=p_sql, charset="utf8")
@@ -9,7 +12,7 @@ def db_init(u_sql, p_sql):
     conn.query('use bilibili')
     conn.query('create table if not exists up_info (mid int NOT NULL PRIMARY KEY,name varchar(200),sex varchar(20),regtime int,birthday varchar(200),place varchar(20),description varchar(200),fans int,attention int,sign varchar(200),level int,vipStatus int,article int,nameplant_name varchar(200),nameplant_level varchar(200),nameplant_condition varchar(200),official_type int,official_verify varchar(200));')
     conn.query('create table  if not exists up_relation (mid int, attention int);')
-    conn.query('create table  if not exists up_waiting (no int AUTO_INCREMENT PRIMARY KEY , mid int NOT NULL);')
+    conn.query('create table  if not exists up_waiting (no int AUTO_INCREMENT PRIMARY KEY , mid int NOT NULL, CONSTRAINT MID_NOT UNIQUE (mid));')
     conn.close()
 
 def insert_into_up_info(u_sql, p_sql, data):
@@ -32,33 +35,57 @@ def insert_into_up_info(u_sql, p_sql, data):
     official_verify_type = data['official_verify']['type']
     official_verify_desc = "'"+data['official_verify']['desc']+"'"
     r_list = data['attentions']
-
     conn = pymysql.connect(host='localhost',port=3306,user=u_sql,password=p_sql, db='bilibili', charset="utf8")
     row = conn.cursor()
-    insert_into_up_info ="insert into up_info values(%s,%s,%s,%d,%s,%s,%s,%d,%d,%s,%d,%d,%d,%s,%s,%s,%d,%s);"
-    insert_into_relation = "insert into up_relation values(%d,%d);"
-    insert_into_waiting  = "insert into up_waiting values(%d,%d);"
-    get_info_from_up_info = "select * from up_info where mid = %d;"
- 
+    insert_into_up_info1 ="insert into up_info values(%s,%s,%s,%d,%s,%s,%s,%d,%d,%s,%d,%d,%d,%s,%s,%s,%d,%s);" 
     try:
-        row.execute(insert_into_up_info % (mid,name,sex,regtime,birthday,place,description,fans,attention,sign,level_info_current_level,vipStatus,article,nameplate_name,nameplate_level,nameplant_condition,official_verify_type,official_verify_desc))
+        row.execute(insert_into_up_info1 % (mid,name,sex,regtime,birthday,place,description,fans,attention,sign,level_info_current_level,vipStatus,article,nameplate_name,nameplate_level,nameplant_condition,official_verify_type,official_verify_desc))
     except Exception as e:
-        print ("此up主已抓取？报错如下：")
         print (e)
-    for i in r_list:
-        try:
-            row.execute(insert_into_relation % (int(mid),int(i)))
-        except Exception as e:
-            print ("此对应关系已记录？报错如下：")
-            print (e)
-        try:
-            row.execute(get_info_from_up_info % int(i))
-            print ("id为%d的用户已抓取，不用再次入库" % int(i))
-        except Exception as e:
-            row.execute(insert_into_waiting % (int(), int(i)))
-            print ("UPID=%d已加入waiting库，待抓取" % int(i))
-            print (e)
-     
     row.close()
     conn.commit()
     conn.close()
+
+    flowing = Pool(5)
+    for i in r_list:
+        flowing.apply_async(func=insert_waiting, args=(u_sql, p_sql, mid, i, ))
+    print ("%s关注的up主开始对比" % mid)
+    flowing.close()
+    flowing.join()
+
+
+
+
+def insert_waiting(u_sql, p_sql, mid, i):
+    print ("%s进程已启动,开始处理mid为%s的用户" % (multiprocessing.current_process().name, i))
+    #time.sleep(1)
+    conn = pymysql.connect(host='localhost', port=3306, user=u_sql, password=p_sql, db='bilibili', charset="utf8")
+    row = conn.cursor()
+    insert_into_relation = "insert into up_relation values(%d,%d);"
+    insert_into_waiting  = "insert into up_waiting values(%d,%d);"
+    get_info_from_up_info = "select * from up_info where mid = %d;"
+    try:
+        row.execute(insert_into_relation % (int(mid),i))
+    except Exception as e:
+        print (e)
+    conn.commit()
+    if ( row.execute(get_info_from_up_info % i) == 1 ):
+        print ("id为%d的用户已抓取，不用再次入库" % i)
+    else:
+    #except Exception as e:
+        row.execute(insert_into_waiting % (int(), i))
+        print ("UPID=%d已加入waiting库，待抓取" % i)
+        conn.commit()
+    row.close()
+    conn.close()
+    return 0
+
+if __name__ == '__main__':
+    u_sql = 'pylocal'
+    p_sql = '123456'
+    UPID = '592761'
+    time.sleep(1)
+    url = ('https://api.bilibili.com/cardrich?mid=' + UPID)
+    json = requests.get(url).json()['data']['card']
+    db_init(u_sql, p_sql)
+    insert_into_up_info(u_sql, p_sql, json)
