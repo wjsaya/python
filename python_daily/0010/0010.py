f0d85a1c25d6f9304a9f96b90e4acea1d7359862
wjsaya <sayawj@163.com>
2018-06-26 19:14:45 +0800

    Changes to be committed:

--- python_daily/0010/0010.py
+++ python_daily/0010/0010.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python
 #coding:utf-8
-
-
-from PIL import Image,ImageDraw,ImageFont,ImageFilter
 import random
 import string
+from PIL import Image, ImageDraw, ImageFont, ImageFilter
 
 def get_code(leng):
     code = ''
     for i in range(0,int(leng)):
         code = code + str(random.choice(string.ascii_letters[:]))
     return(code)
-
-
+ 
 def put_image(code):
-    print (code)
+    width = 100 * 4
+    height = 100
+    image = Image.new('RGB', (width, height), (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
 
+    draw =ImageDraw.Draw(image)
+    draw.line((0,0) +image.size, fill=128)
 
-
-
+    Font4 = ImageFont.truetype("C:\Windows\Fonts\SHOWG.TTF",50)  
+    draw.text([40, 10], code, font = Font4)  
+    image.show()
 
 if __name__ == '__main__':
     ver_code = get_code(4)
